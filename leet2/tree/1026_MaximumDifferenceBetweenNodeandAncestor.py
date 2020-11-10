96a34b07865a7251be849d3563fbe3fa7e7ee820
ruan65 <ruan65@gmail.com>
2020-11-10 03:01:58 +0300

    better

--- leet2/tree/1026_MaximumDifferenceBetweenNodeandAncestor.py
+++ leet2/tree/1026_MaximumDifferenceBetweenNodeandAncestor.py
@@ -6,17 +6,27 @@ class TreeNode:
         self.right = right
 
 
-class Solution:
-    res = 0
+# class Solution:
+#     res = 0
+#
+#     def maxAncestorDiff(self, root: TreeNode) -> int:
+#         def diff(nd, mx, mn):
+#             if not nd:
+#                 return
+#             self.res = max(self.res, abs(mx - nd.val), abs(mn - nd.val))
+#             mx, mn = max(mx, nd.val), min(mn, nd.val)
+#             diff(nd.left, mx, mn)
+#             diff(nd.right, mx, mn)
+#
+#         diff(root, root.val, root.val)
+#         return self.res
 
+class Solution:
     def maxAncestorDiff(self, root: TreeNode) -> int:
         def diff(nd, mx, mn):
             if not nd:
-                return
-            self.res = max(self.res, abs(mx - nd.val), abs(mn - nd.val))
+                return mx - mn
             mx, mn = max(mx, nd.val), min(mn, nd.val)
-            diff(nd.left, mx, mn)
-            diff(nd.right, mx, mn)
+            return max(diff(nd.left, mx, mn), diff(nd.right, mx, mn))
 
-        diff(root, root.val, root.val)
-        return self.res
+        return diff(root, root.val, root.val)
